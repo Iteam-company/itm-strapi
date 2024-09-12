bc43aa6d6a225abb7beca299882d1b3794b9eff9
Igor <sergienko339@gmail.com>
2024-09-12 17:06:53 +0300

    feat: strapi custom API (contact_us)

--- src/api/about-us/controllers/about-us.ts
+++ src/api/about-us/controllers/about-us.ts
@@ -2,6 +2,24 @@
  * about-us controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::about-us.about-us');
+export default factories.createCoreController(
+  "api::about-us.about-us",
+  ({ s }) => ({
+    async find(ctx) {
+      const { query } = ctx;
+
+      return await s.query("api::about-us.about-us").find({
+        ...query,
+        populate: {
+          comments: {
+            populate: {
+              commentInfo: true,
+            },
+          },
+        },
+      });
+    },
+  })
+);
