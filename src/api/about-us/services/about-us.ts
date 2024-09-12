7bad7c1f98e63974d6371835205b46868e33a716
Igor <sergienko339@gmail.com>
2024-09-12 17:35:52 +0300

    fix: strapi query

--- src/api/about-us/services/about-us.ts
+++ src/api/about-us/services/about-us.ts
@@ -8,13 +8,12 @@ export default factories.createCoreService(
   "api::about-us.about-us",
   ({ strapi }) => ({
     async findWithRelations(query) {
-      // Custom query with deep population
       return await strapi.entityService.findMany("api::about-us.about-us", {
         ...query,
         populate: {
           comments: {
             populate: {
-              commentInfo: true, // Populate nested relation
+              commentInfo: true,
             },
           },
         },
