683f48bc4373a3183c1aaf05e95546401f64098e
Yoland172 <volodvoldimer@gmail.com>
2025-01-28 18:05:09 +0200

    feat: config cloudinary prodider

--- config/plugins.ts
+++ config/plugins.ts
@@ -18,6 +18,29 @@ export default ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
+        params: {
+          folder: "strapi-assets",
+          resource_type: "auto",
+          transformation: {
+            format: {
+              thumbnail: {
+                width: 150,
+                height: 150,
+                crop: "fill",
+              },
+              small: {
+                width: 320,
+                height: 320,
+                crop: "fill",
+              },
+              large: {
+                width: 1000,
+                height: 1000,
+                crop: "fill",
+              },
+            },
+          },
+        },
       },
       actionOptions: {
         upload: {},
