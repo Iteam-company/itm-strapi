8a8f062d8d36068db186b6c63d48e7f33bc057a0
krlchk <145658577+krlchk@users.noreply.github.com>
2026-04-13 15:55:13 +0300

    fix: rclone fix backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,13 +9,18 @@ mkdir -p backups
 
 npx strapi export --no-encrypt --exclude files --file backups/backup-$DATE
 
-echo "Installing rclone..."
-curl https://rclone.org/install.sh | bash
+echo "Downloading rclone..."
+
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cp rclone-*-linux-amd64/rclone ./rclone
+chmod +x ./rclone
 
 mkdir -p ~/.config/rclone
 echo "$RCLONE_CONFIG" > ~/.config/rclone/rclone.conf
 
 echo "Uploading backup to Google Drive..."
-rclone copy backups gdrive:strapi-backups
 
-echo "Backup uploaded successfully"
+./rclone copy backups gdrive:strapi-backups
+
+echo "Backup uploaded successfully!"
